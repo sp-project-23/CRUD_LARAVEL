017c473be3490775e57bd8d88036e02422988ef4
Sourav <sp.manick19@gmail.com>
2025-10-14 21:31:03 +0530

    do

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('categories',[CategoryController::class,'index']);
-Route::get('add',[CategoryController::class,'create']);
-Route::post('save',[CategoryController::class,'store']);
-Route::get('edit/{id}',[CategoryController::class,'edit']);
-Route::put('update/{id}',[CategoryController::class,'update']);
-Route::delete('remove/{id}',[CategoryController::class,'delete']);
+Route::controller(CategoryController::class)->group(function () {
+    Route::get('/categories','index');
+    Route::get('/add','create');
+    Route::post('/save','store');
+    Route::get('/edit/{id}','edit');
+    Route::put('/update/{id}','update');
+    Route::delete('/remove/{id}','delete');
+});
